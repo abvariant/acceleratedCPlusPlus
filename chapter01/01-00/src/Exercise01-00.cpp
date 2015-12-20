4ddf6f224d1efda0eb58e0ea376d884f956ac50b
Adrian Bell <adrian@paperfish.co.uk>
2015-12-20 12:26:08 +0000

    First program from this chapter implemented.

--- chapter01/01-00/src/Exercise01-00.cpp
+++ chapter01/01-00/src/Exercise01-00.cpp
@@ -1,7 +1,18 @@
+// Ask for a person's name, and greet the person.
+
 #include <iostream>
+#include <string>
 
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+	// Ask for the person's name
+	std::cout << "Please enter your first name: " << std::endl;
+
+	// Read the name
+	std::string name;	// define name
+	std::cin >> name;	// read into name
+
+	// Write greeting
+	std::cout << "Hello, " << name << "!" << std::endl;
 	return 0;
 }	
