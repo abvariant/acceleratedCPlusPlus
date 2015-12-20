077312e304991de555457d1e9c77c3c307af00ba
Adrian Bell <adrian@paperfish.co.uk>
2015-12-20 12:13:01 +0000

    Initial commit.

---
+++ chapter00/0-3/src/Exercise0-3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "\tThis (\") is a quote, and this (\\) is a backslash." << std::endl;
+	std::cout << "\t\tThis (\") is a quote, and this (\\) is a backslash." << std::endl;
+	std::cout << "\t\t\tThis (\") is a quote, and this (\\) is a backslash." << std::endl;
+	return 0;
+}	
---
+++ chapter01/01-00/src/CMakeFiles/Exercise01-00.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ade/SYN/programming/git/acceleratedCPlusPlus/chapter01/01-00/src/Exercise01-00.cpp" "/home/ade/SYN/programming/git/acceleratedCPlusPlus/chapter01/01-00/src/CMakeFiles/Exercise01-00.dir/Exercise01-00.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
